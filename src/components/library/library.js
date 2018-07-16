388a911648162a242c55c989e3ce2dc7654fe272
Code <Code@batc.local>
2018-07-16 08:54:52 -0600

    integrated font awesome and built custom icon component

--- src/components/library/library.js
+++ src/components/library/library.js
@@ -6,7 +6,7 @@ class Library extends Component {
     render() {
        return (
              <div className='library'>
-                <h1 className='library_title'>Course Library</h1>
+                <h1 className='library__title'>Course Library</h1>
                 <LibraryCourse />
                 <LibraryCourse />
                 <LibraryCourse />
--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 
+import Icon from '../icon';
+
 class LibraryCourse extends Component {
     render() {
        return (
              <div className='library-course'>
-                <label className='library-course_title'>Problem Solving</label>
-                {/* icon component */}
+                <label className='library-course__title'>Problem Solving</label>
+                { Icon('fas fa-check', 'library-course__icon') }
                 {/* arrow component */}
                 {/* action button component*/}
-                <div className='library-course_description'>
+                <div className='library-course__description'>
                     <label>Course Description</label>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed venenatis tortor sed mi malesuada mattis. Duis rutrum sed dolor eu consequat. Aliquam egestas convallis eros vel tempor. Donec viverra congue ultrices. Maecenas sollicitudin leo lectus, ac posuere tortor fringilla et. Etiam fermentum quam at odio sodales cursus.</p>
                 </div>
